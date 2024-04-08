082efd9c91012759d5a8a2b5608e4ba380182717
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-08 20:43:23 -0300

    concluido

---
+++ insert_user.php
@@ -0,0 +1,23 @@
+<?php
+//Recebendo dados do formulário
+
+$nome = $_POST ['nome'];
+$login = $_POST ['login'];
+$setor = $_POST ['setor'];
+$senha = $_POST ['senha'];
+
+
+echo "$nome";
+echo "<br>";
+echo "$login";
+echo "<br>";
+echo "$setor";
+echo "<br>";
+echo "$senha";
+echo "<br>";
+
+
+print_r($_POST);
+
+
+?>
