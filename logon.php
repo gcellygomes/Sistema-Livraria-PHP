8cdcce5e7d99f8ce42d9b6e005ee5a74b8bb942e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-06 21:21:16 -0300

    aula dia 06 maio 2023

--- logon.php
+++ logon.php
@@ -11,7 +11,7 @@ include 'conexao.php';
 
 //selecionar os dados no banco de dados
 
-$select = "SELECT * FROM tb_user WHERE LOGIN = '$nome'";
+$select = "SELECT * FROM tb_user WHERE login = '$nome'";
 
 $query= mysqli_query($conexao, $select);
 
@@ -20,17 +20,21 @@ $result = mysqli_fetch_array($query);
 
 // Dados do banco armazenado na variável
 
-$name_banco = $result['setor'];
-$senha_banco = $result['senha'];
+$name_banco = $result['login'];
+$senha_banco = $result['Senha'];
+
+
 
 //Comparação para acessar o Sistema
 
+
 if ($nome == $name_banco && $senha == $senha_banco) {
 
-    header('location: cadastro.html');
-}else{
+    header('location: cadastro_cliente.html');
 
-    echo "<script>alert('Usuario Invalido!'); history.back();</script>";
+} else{
+
+    echo "<script>alert ('Usuario Invalido!'); history.back()</script>";
 
 }
 
